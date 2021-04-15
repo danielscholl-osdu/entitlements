b488ada1634f2e324b9c90e8d4984945dfb13df1
Mingyang Zhu <mzhu9@slb.com>
2021-04-15 17:52:34 +0000

    return 503 instead 401 if querying rebuilt cache timeout and adjust the Retry parameter based on the load test cosmos latency

--- provider/entitlements-v2-azure/src/main/java/org/opengroup/osdu/entitlements/v2/azure/service/GroupCacheServiceAzure.java
+++ provider/entitlements-v2-azure/src/main/java/org/opengroup/osdu/entitlements/v2/azure/service/GroupCacheServiceAzure.java
@@ -1,38 +1,101 @@
 package org.opengroup.osdu.entitlements.v2.azure.service;
 
+import io.github.resilience4j.retry.Retry;
 import lombok.RequiredArgsConstructor;
 import org.opengroup.osdu.core.common.cache.ICache;
+import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.entitlements.v2.azure.service.metrics.hitsnmisses.HitsNMissesMetricService;
 import org.opengroup.osdu.entitlements.v2.model.EntityNode;
 import org.opengroup.osdu.entitlements.v2.model.ParentReference;
 import org.opengroup.osdu.entitlements.v2.model.ParentReferences;
 import org.opengroup.osdu.entitlements.v2.service.GroupCacheService;
-import org.opengroup.osdu.entitlements.v2.azure.service.metrics.hitsnmisses.HitsNMissesMetricService;
 import org.opengroup.osdu.entitlements.v2.spi.retrievegroup.RetrieveGroupRepo;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class GroupCacheServiceAzure implements GroupCacheService {
+    private final JaxRsDpsLog log;
     private final RetrieveGroupRepo retrieveGroupRepo;
     private final ICache<String, ParentReferences> redisGroupCache;
     private final HitsNMissesMetricService metricService;
+    private final RedissonClient redissonClient;
+    private final Retry retry;
+    @Value("${redisson.lock.acquisition.timeout}")
+    private int redissonLockAcquisitionTimeOut;
+    @Value("${redisson.lock.expiration}")
+    private int redissonLockExpiration;
 
     @Override
     public Set<ParentReference> getFromPartitionCache(String requesterId, String partitionId) {
         String key = String.format("%s-%s", requesterId, partitionId);
         ParentReferences parentReferences = redisGroupCache.get(key);
         if (parentReferences == null) {
-            metricService.sendMissesMetric();
-            EntityNode entityNode = EntityNode.createMemberNodeForNewUser(requesterId, partitionId);
-            Set<ParentReference> allParents = retrieveGroupRepo.loadAllParents(entityNode).getParentReferences();
-            parentReferences = new ParentReferences();
-            parentReferences.setParentReferencesOfUser(allParents);
-            redisGroupCache.put(key, parentReferences);
+            RLock cacheEntryLock = redissonClient.getLock(key);
+            return lockCacheEntryAndRebuild(cacheEntryLock, key, requesterId, partitionId);
         } else {
             metricService.sendHitsMetric();
+            return parentReferences.getParentReferencesOfUser();
         }
-        return parentReferences.getParentReferencesOfUser();
     }
+
+    /**
+     The unblock function may throw exception when cache update takes longer than the lock expiration time,
+     so when the time it tries to unlock the lock has already expired or re-acquired by another thread. In this case, since the lock is already released, we just
+     log the error message without doing anything further. The log is for the tracking purpose to understand the possibility so we can adjust parameters accordingly.
+     Refer to: https://github.com/redisson/redisson/issues/581
+     */
+    private Set<ParentReference> lockCacheEntryAndRebuild(RLock cacheEntryLock, String key, String requesterId, String partitionId) {
+        boolean locked = false;
+        try {
+            locked = cacheEntryLock.tryLock(redissonLockAcquisitionTimeOut, redissonLockExpiration, TimeUnit.MILLISECONDS);
+            if (locked) {
+                metricService.sendMissesMetric();
+                ParentReferences parentReferences = rebuildCache(requesterId, partitionId);
+                redisGroupCache.put(key, parentReferences);
+                return parentReferences.getParentReferencesOfUser();
+            } else {
+                ParentReferences parentReferences = Retry.decorateSupplier(retry, () -> redisGroupCache.get(key)).get();
+                if (parentReferences == null) {
+                    metricService.sendMissesMetric();
+                } else {
+                    metricService.sendHitsMetric();
+                    return parentReferences.getParentReferencesOfUser();
+                }
+            }
+        } catch (InterruptedException ex) {
+            log.error(String.format("InterruptedException caught when lock the cache key %s: %s", key, ex));
+            Thread.currentThread().interrupt();
+        } finally {
+            if (locked) {
+                try {
+                    cacheEntryLock.unlock();
+                } catch (Exception ex) {
+                    log.warning(String.format("unlock exception: %s", ex));
+                }
+            }
+        }
+        throw new AppException(
+                HttpStatus.SERVICE_UNAVAILABLE.value(),
+                HttpStatus.SERVICE_UNAVAILABLE.getReasonPhrase(),
+                "Failed to get the groups");
+    }
+
+    private ParentReferences rebuildCache(String requesterId, String partitionId) {
+        EntityNode entityNode = EntityNode.createMemberNodeForNewUser(requesterId, partitionId);
+        Set<ParentReference> allParents = retrieveGroupRepo.loadAllParents(entityNode).getParentReferences();
+        ParentReferences parentReferences = new ParentReferences();
+        parentReferences.setParentReferencesOfUser(allParents);
+        return parentReferences;
+    }
+
 }
--- provider/entitlements-v2-azure/src/test/java/org/opengroup/osdu/entitlements/v2/azure/service/GroupCacheServiceAzureTest.java
+++ provider/entitlements-v2-azure/src/test/java/org/opengroup/osdu/entitlements/v2/azure/service/GroupCacheServiceAzureTest.java
@@ -1,45 +1,113 @@
 package org.opengroup.osdu.entitlements.v2.azure.service;
 
+import io.github.resilience4j.retry.Retry;
+import org.awaitility.Duration;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.AdditionalAnswers;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.opengroup.osdu.core.common.cache.ICache;
+import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.entitlements.v2.azure.service.metrics.hitsnmisses.HitsNMissesMetricService;
 import org.opengroup.osdu.entitlements.v2.model.EntityNode;
 import org.opengroup.osdu.entitlements.v2.model.ParentReference;
 import org.opengroup.osdu.entitlements.v2.model.ParentReferences;
 import org.opengroup.osdu.entitlements.v2.model.ParentTreeDto;
-import org.opengroup.osdu.entitlements.v2.azure.service.metrics.hitsnmisses.HitsNMissesMetricService;
 import org.opengroup.osdu.entitlements.v2.spi.retrievegroup.RetrieveGroupRepo;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.embedded.RedisServer;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {"spring.application.name=test",
+        "redis.redisson.lock.acquisition.timeout=10",
+        "redisson.lock.expiration=5000",
+        "cache.retry.max=15",
+        "cache.retry.interval=200",
+        "cache.retry.random.factor=0.1"})
+@RunWith(SpringRunner.class)
 public class GroupCacheServiceAzureTest {
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        public RedissonClient mockRedissonClient() {
+            Config config = new Config();
+            config.useSingleServer().setAddress(String.format("redis://%s:%d", "localhost", 7000))
+                    .setPassword("pass")
+                    .setDatabase(0)
+                    .setTimeout(100000)
+                    .setClientName("test");
+            return Redisson.create(config);
+        }
+    }
 
     private Set<ParentReference> parents = new HashSet<>();
     private ParentReferences parentReferences = new ParentReferences();
     private EntityNode requester;
 
-    @Mock
+    @MockBean
     private RetrieveGroupRepo retrieveGroupRepo;
-    @Mock
+    @MockBean
+    private HitsNMissesMetricService metricService;
+    @MockBean
     private ICache<String, ParentReferences> redisGroupCache;
+    @MockBean
+    private JaxRsDpsLog log;
     @Mock
     private ParentTreeDto parentTreeDto;
-    @Mock
-    private HitsNMissesMetricService metricService;
 
-    @InjectMocks
+    @Autowired
+    private RedissonClient redissonClient;
+    @Autowired
+    private Retry retry;
+    @Autowired
     private GroupCacheServiceAzure sut;
+
+    private static RedisServer redisServer;
+
+    @BeforeClass
+    public static void setupClass() {
+        redisServer = RedisServer.builder().port(7000).setting("requirepass pass").build();
+        redisServer.start();
+    }
+
+    @AfterClass
+    public static void end() {
+        redisServer.stop();
+    }
 
     @Before
     public void setup() {
@@ -83,5 +151,83 @@ public class GroupCacheServiceAzureTest {
         assertEquals(this.parents, result);
         verify(this.retrieveGroupRepo, times(0)).loadAllParents(this.requester);
         verify(this.metricService, times(1)).sendHitsMetric();
+    }
+
+    @Test
+    public void shouldOnlyOneThreadRebuildCacheInConcurrentScenarioAndOtherThreadWaitAndReturnTheRebuiltCache() throws InterruptedException {
+        List<ParentReferences> cacheValues = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            cacheValues.add(null);
+        }
+        cacheValues.add(this.parentReferences);
+        when(this.redisGroupCache.get("requesterId-dp")).thenAnswer(AdditionalAnswers.returnsElementsOf(cacheValues));
+        when(this.retrieveGroupRepo.loadAllParents(this.requester)).thenAnswer((Answer<ParentTreeDto>) invocationOnMock -> {
+            await().atLeast(Duration.FIVE_SECONDS);
+            return parentTreeDto;
+        });
+        when(this.parentTreeDto.getParentReferences()).thenReturn(this.parents);
+
+        int threads = 3;
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        List<Callable<Set<ParentReference>>> tasks = new ArrayList<>();
+
+        for (int i = 0; i < threads; i++) {
+            Callable<Set<ParentReference>> task = () -> sut.getFromPartitionCache("requesterId", "dp");
+            tasks.add(task);
+        }
+
+        List<Future<Set<ParentReference>>> responses = executor.invokeAll(tasks);
+        executor.shutdown();
+
+        verify(this.retrieveGroupRepo, times(1)).loadAllParents(this.requester);
+        responses.forEach(result -> {
+            try {
+                assertEquals(this.parents, result.get());
+            } catch (InterruptedException | ExecutionException e) {
+                fail("No exception expected");
+            }
+        });
+    }
+
+    @Test
+    public void shouldReturnEmptyIfTimeout() throws InterruptedException {
+        when(this.redisGroupCache.get("requesterId-dp")).thenReturn(null);
+        when(this.retrieveGroupRepo.loadAllParents(this.requester)).thenAnswer((Answer<ParentTreeDto>) invocationOnMock -> {
+            await().atLeast(Duration.FIVE_SECONDS);
+            return parentTreeDto;
+        });
+        when(this.parentTreeDto.getParentReferences()).thenReturn(this.parents);
+
+        int threads = 3;
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        List<Callable<Set<ParentReference>>> tasks = new ArrayList<>();
+
+        for (int i = 0; i < threads; i++) {
+            Callable<Set<ParentReference>> task = () -> sut.getFromPartitionCache("requesterId", "dp");
+            tasks.add(task);
+        }
+
+        List<Future<Set<ParentReference>>> responses = executor.invokeAll(tasks);
+        executor.shutdown();
+
+        verify(this.retrieveGroupRepo, times(1)).loadAllParents(this.requester);
+        assertEquals(1, responses.stream().filter(result -> {
+            try {
+                return this.parents.equals(result.get());
+            } catch (InterruptedException | ExecutionException e) {
+            }
+            return false;
+        }).count());
+        assertEquals(2, responses.stream().filter(result -> {
+            try {
+                result.get();
+            } catch (InterruptedException | ExecutionException e) {
+                AppException appEx = (AppException)e.getCause();
+                if (appEx.getError().getCode() == 503) {
+                    return true;
+                }
+            }
+            return false;
+        }).count());
     }
 }
