407f2463715078e7e4d934cf43b51a5e36888649
zhijie wang <wanzhiji@amazon.com>
2021-06-24 14:39:17 -0700

    inspect check gate for user node exist

--- entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/service/AddMemberService.java
+++ entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/service/AddMemberService.java
@@ -40,7 +40,12 @@ public class AddMemberService {
         log.info(String.format("requested by %s", addMemberServiceDto.getRequesterId()));
         String memberDesId = addMemberDto.getEmail();
         EntityNode memberNode = retrieveGroupRepo.getEntityNode(memberDesId, addMemberServiceDto.getPartitionId()).orElseGet(
-                () -> createNewMemberNode(addMemberDto.getEmail(), memberDesId, addMemberServiceDto.getPartitionId()));
+
+                () -> {
+                    System.out.println("No user Node found, adding it");
+                    return  createNewMemberNode(addMemberDto.getEmail(), memberDesId, addMemberServiceDto.getPartitionId())
+                }
+                );
         EntityNode existingGroupEntityNode = retrieveGroupRepo.groupExistenceValidation(addMemberServiceDto.getGroupEmail(), addMemberServiceDto.getPartitionId());
         EntityNode requesterNode = EntityNode.createMemberNodeForRequester(addMemberServiceDto.getRequesterId(), addMemberServiceDto.getPartitionId());
         if (!permissionService.hasOwnerPermissionOf(requesterNode, existingGroupEntityNode)) {
