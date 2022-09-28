827761f227ef5fcf0007f4e8b3e8bdd17291fffd
Michael Sacco <saccomi@amazon.com>
2022-09-28 11:35:14 -0400

    Added additional accepted regex to Description field

--- entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/model/creategroup/CreateGroupDto.java
+++ entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/model/creategroup/CreateGroupDto.java
@@ -15,20 +15,25 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateGroupDto {
-    @Pattern(regexp = "^[A-Za-z0-9{}_.-]{3,128}$")
+    // TODO: These should be moved to org.opengroup.osdu.core.common.model.storage.validation - ValidationDoc
+    //       constants once we decide on our validation regexes at the forum level (and made public).
+    private static final String NAME_REGEX = "^[A-Za-z0-9{}_.-]{3,128}$";
+    private static final String FREE_TEXT_REGEX = "^[A-Za-z0-9 _.-/,;:\'\"!@&+%#$]{0,255}$";
+
+    @Pattern(regexp = NAME_REGEX)
     @NotNull
     private String name;
 
-    @Pattern(regexp = "^[A-Za-z0-9 _.-]{0,255}$")
+    @Pattern(regexp = FREE_TEXT_REGEX)
     private String description;
 
     public static EntityNode createGroupNode(CreateGroupDto dto, String domain, String partitionId) {
         return EntityNode.builder()
-                .name(dto.name.toLowerCase())
-                .nodeId(String.format("%s@%s", dto.name.toLowerCase(), domain.toLowerCase()))
-                .description(Strings.isNullOrEmpty(dto.description) ? "" : dto.description)
-                .type(NodeType.GROUP)
-                .dataPartitionId(partitionId)
-                .build();
+                         .name(dto.name.toLowerCase())
+                         .nodeId(String.format("%s@%s", dto.name.toLowerCase(), domain.toLowerCase()))
+                         .description(Strings.isNullOrEmpty(dto.description) ? "" : dto.description)
+                         .type(NodeType.GROUP)
+                         .dataPartitionId(partitionId)
+                         .build();
     }
 }
