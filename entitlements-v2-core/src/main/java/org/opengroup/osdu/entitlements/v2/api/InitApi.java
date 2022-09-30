08d064e8e88d6c4af9c076fab622f698290b3f70
Rustam Lotsmanenko (EPAM) <rustam_lotsmanenko@epam.com>
2022-09-30 16:39:41 +0000

    Upgrade bootstrapping of initial accounts, added body to provisioning request

--- entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/api/InitApi.java
+++ entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/api/InitApi.java
@@ -1,10 +1,13 @@
 package org.opengroup.osdu.entitlements.v2.api;
 
+import org.opengroup.osdu.entitlements.v2.model.init.InitServiceDto;
 import org.opengroup.osdu.entitlements.v2.service.TenantInitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +18,9 @@ public class InitApi {
 
     @PostMapping("/tenant-provisioning")
     @PreAuthorize("@authorizationFilter.hasAnyPermission()")
-    public ResponseEntity<Void> initiateTenant() {
+    public ResponseEntity<InitServiceDto> initiateTenant(@RequestBody(required = false) InitServiceDto initServiceDto) {
         tenantInitService.createDefaultGroups();
-        tenantInitService.bootstrapInitialAccounts();
-        return ResponseEntity.ok().build();
+        tenantInitService.bootstrapInitialAccounts(initServiceDto);
+        return new ResponseEntity<>(initServiceDto, HttpStatus.OK);
     }
 }
