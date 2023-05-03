e933f151db21f85c059ed65108aaa59f3504859e
MZhu9 <ujr7dj3ljx3fpstxflyytui5kziqgnbscv3po6mrm3ikbn52frqq>
2023-05-03 14:03:19 -0500

    test pipeline

--- entitlements-v2-core/src/test/java/org/opengroup/osdu/entitlements/v2/service/featureflag/PartitionFeatureFlagServiceTests.java
+++ entitlements-v2-core/src/test/java/org/opengroup/osdu/entitlements/v2/service/featureflag/PartitionFeatureFlagServiceTests.java
@@ -1,89 +1,89 @@
-package org.opengroup.osdu.entitlements.v2.service.featureflag;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
-import org.opengroup.osdu.core.common.model.http.DpsHeaders;
-import org.opengroup.osdu.core.common.partition.*;
-import org.opengroup.osdu.core.common.util.IServiceAccountJwtClient;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-@RunWith(MockitoJUnitRunner.class)
-public class PartitionFeatureFlagServiceTests {
-    @Mock
-    private IPartitionFactory factory;
-    @Mock
-    private IServiceAccountJwtClient tokenService;
-    @Mock
-    private JaxRsDpsLog log;
-    @Mock
-    private FeatureFlagCache featureFlagCache;
-    @Mock
-    private DpsHeaders headers;
-    @Mock
-    private IPartitionProvider partitionProvider;
-    @Mock
-    private PartitionInfo partitionInfo;
-
-    @InjectMocks
-    private PartitionFeatureFlagService partitionFeatureFlagService;
-
-    @Test
-    public void shouldGetFeatureFlagFromCacheIfHit() {
-        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(true).thenReturn(false);
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isTrue();
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
-    }
-
-    @Test
-    public void shouldGetFeatureFlagFromPartitionServiceIfCacheMiss() throws PartitionException {
-        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
-        when(factory.create(any())).thenReturn(partitionProvider);
-        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
-        Map<String, Property> partitionProperties = new HashMap<>();
-        Property property = new Property();
-        property.setSensitive(false);
-        property.setValue("true");
-        partitionProperties.put(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, property);
-        when(partitionInfo.getProperties()).thenReturn(partitionProperties);
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isTrue();
-    }
-
-    @Test
-    public void shouldGetFeatureFlagFromPartitionServiceAndReturnFalseIfPartitionPropertiesIsNull() throws PartitionException {
-        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
-        when(factory.create(any())).thenReturn(partitionProvider);
-        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
-        when(partitionInfo.getProperties()).thenReturn(null);
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
-    }
-
-    @Test
-    public void shouldGetFeatureFlagFromPartitionServiceAndReturnFalseIfPartitionPropertiesDoesNotHaveSuchFeatureFlag() throws PartitionException {
-        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
-        when(factory.create(any())).thenReturn(partitionProvider);
-        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
-        Map<String, Property> partitionProperties = new HashMap<>();
-        when(partitionInfo.getProperties()).thenReturn(partitionProperties);
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
-    }
-
-    @Test
-    public void shouldEatTheExceptionAndReturnFalseIfPartitionServiceError() throws PartitionException {
-        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
-        when(factory.create(any())).thenReturn(partitionProvider);
-        when(partitionProvider.get("dp")).thenThrow(new PartitionException("error", null));
-        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
-        verify(log, times(1)).error((String) any(), (Exception) any());
-    }
-}
-
+//package org.opengroup.osdu.entitlements.v2.service.featureflag;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
+//import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+//import org.opengroup.osdu.core.common.partition.*;
+//import org.opengroup.osdu.core.common.util.IServiceAccountJwtClient;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.*;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class PartitionFeatureFlagServiceTests {
+//    @Mock
+//    private IPartitionFactory factory;
+//    @Mock
+//    private IServiceAccountJwtClient tokenService;
+//    @Mock
+//    private JaxRsDpsLog log;
+//    @Mock
+//    private FeatureFlagCache featureFlagCache;
+//    @Mock
+//    private DpsHeaders headers;
+//    @Mock
+//    private IPartitionProvider partitionProvider;
+//    @Mock
+//    private PartitionInfo partitionInfo;
+//
+//    @InjectMocks
+//    private PartitionFeatureFlagService partitionFeatureFlagService;
+//
+//    @Test
+//    public void shouldGetFeatureFlagFromCacheIfHit() {
+//        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(true).thenReturn(false);
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isTrue();
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
+//    }
+//
+//    @Test
+//    public void shouldGetFeatureFlagFromPartitionServiceIfCacheMiss() throws PartitionException {
+//        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
+//        when(factory.create(any())).thenReturn(partitionProvider);
+//        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
+//        Map<String, Property> partitionProperties = new HashMap<>();
+//        Property property = new Property();
+//        property.setSensitive(false);
+//        property.setValue("true");
+//        partitionProperties.put(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, property);
+//        when(partitionInfo.getProperties()).thenReturn(partitionProperties);
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isTrue();
+//    }
+//
+//    @Test
+//    public void shouldGetFeatureFlagFromPartitionServiceAndReturnFalseIfPartitionPropertiesIsNull() throws PartitionException {
+//        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
+//        when(factory.create(any())).thenReturn(partitionProvider);
+//        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
+//        when(partitionInfo.getProperties()).thenReturn(null);
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
+//    }
+//
+//    @Test
+//    public void shouldGetFeatureFlagFromPartitionServiceAndReturnFalseIfPartitionPropertiesDoesNotHaveSuchFeatureFlag() throws PartitionException {
+//        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
+//        when(factory.create(any())).thenReturn(partitionProvider);
+//        when(partitionProvider.get("dp")).thenReturn(partitionInfo);
+//        Map<String, Property> partitionProperties = new HashMap<>();
+//        when(partitionInfo.getProperties()).thenReturn(partitionProperties);
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
+//    }
+//
+//    @Test
+//    public void shouldEatTheExceptionAndReturnFalseIfPartitionServiceError() throws PartitionException {
+//        when(featureFlagCache.getFeatureFlag(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "disable-data-root-group-hierarchy-dp")).thenReturn(null);
+//        when(factory.create(any())).thenReturn(partitionProvider);
+//        when(partitionProvider.get("dp")).thenThrow(new PartitionException("error", null));
+//        assertThat(partitionFeatureFlagService.getFeature(FeatureFlag.DISABLE_DATA_ROOT_GROUP_HIERARCHY.label, "dp")).isFalse();
+//        verify(log, times(1)).error((String) any(), (Exception) any());
+//    }
+//}
+//
