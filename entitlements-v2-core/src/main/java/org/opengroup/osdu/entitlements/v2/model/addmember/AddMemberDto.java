a9e71d830b8465f38da7129e66b697118dbd51ea
IshaKumari <isha.kumari@ibm.com>
2024-11-26 23:19:40 +0530

    Added validation to ensure the email field is not blank.

--- entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/model/addmember/AddMemberDto.java
+++ entitlements-v2-core/src/main/java/org/opengroup/osdu/entitlements/v2/model/addmember/AddMemberDto.java
@@ -1,6 +1,7 @@
 package org.opengroup.osdu.entitlements.v2.model.addmember;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import org.opengroup.osdu.entitlements.v2.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,7 +20,7 @@ import jakarta.validation.constraints.NotNull;
 public class AddMemberDto {
 
     @Schema(description = "Email Id of the member")
-    @NotNull
+    @NotBlank(message = "must not be blank or null") //error message: "email  must not be blank or null"
     private String email;
 
     @Schema(description = "Role of the member")
