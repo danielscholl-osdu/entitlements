368e2910e53027359f258b712b7c512fa214c733
Daniel Scholl <daniel.scholl@microsoft.com>
2026-09-15 22:17:33 -0500

    test(azure): match the varargs authorization stub in the workflow test

--- provider/entitlements-v2-azure/src/test/java/org/opengroup/osdu/entitlements/v2/azure/workflow/CreateMembershipsWorkflowSinglePartitionTest.java
+++ provider/entitlements-v2-azure/src/test/java/org/opengroup/osdu/entitlements/v2/azure/workflow/CreateMembershipsWorkflowSinglePartitionTest.java
@@ -127,7 +127,7 @@ public class CreateMembershipsWorkflowSinglePartitionTest {
         tenantInfo.setDataPartitionId("common");
         tenantInfo.setServiceAccount("service_principal.com");
         Mockito.when(tenantFactory.getTenantInfo("common")).thenReturn(tenantInfo);
-        when(authService.isCurrentUserAuthorized(any(), any())).thenReturn(true);
+        when(authService.isCurrentUserAuthorized(any(), any(String[].class))).thenReturn(true);
         when(redisGroupCache.getLock(any())).thenReturn(cacheLock);
         when(cacheLock.tryLock(anyLong(), anyLong(), any())).thenReturn(true);
     }
